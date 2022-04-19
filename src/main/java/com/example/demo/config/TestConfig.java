859ad7c6a2e42727fde3833f3503dcd31553e6c3
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-04-19 12:35:38 -0300

    creating a DatabaseInitializer

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -1,83 +1,18 @@
 package com.example.demo.config;
 
-import com.example.demo.entities.Anime;
-import com.example.demo.entities.Score;
-import com.example.demo.entities.User;
-import com.example.demo.services.AnimeService;
-import com.example.demo.services.ScoreService;
-import com.example.demo.services.UserService;
+import com.example.demo.database.DatabaseInitializer;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @RequiredArgsConstructor
-public class TestConfig implements CommandLineRunner {
+@Profile("test")
+public class TestConfig{
 
-    private final AnimeService animeService;
-    private final UserService userService;
-    private final ScoreService scoreService;
+    private final DatabaseInitializer databaseInitializer;
 
-    @Override
-    public void run(String... args) throws Exception {
-        Anime anime1 = new Anime(
-                null,
-                "Spy x Family",
-                new String[]{"Clover Works", "Wit Studio"},
-                new String[]{"Comedy", "Romance", "Slice of life"},
-                "Tatsuya Endo"
-        );
-
-        Anime anime2 = new Anime(
-                null,
-                "One Piece",
-                new String[]{"Toei Animation"},
-                new String[]{"Comedy", "Shounen", "Adventure"},
-                "Eichiro Oda"
-        );
-
-        Anime anime3 = new Anime(
-                null,
-                "Steins;Gate",
-                new String[]{"White Fox"},
-                new String[]{"Sci-fi", "Thriller", "Drama"},
-                "Jukki Hanada"
-        );
-
-        animeService.saveAll(Arrays.asList(anime1, anime2, anime3));
-
-        User user1 = new User(
-                null,
-                "Luiz Adelaide",
-                "Luiz@Luiz.com",
-                "2241",
-                Arrays.asList(anime2, anime1)
-        );
-
-        User user2 = new User(
-                null,
-                "Joao Camara",
-                "Joao@Joao.com",
-                "6666",
-                Arrays.asList(anime1)
-        );
-
-        User user3 = new User(
-                null,
-                "Nathalia Adelaide",
-                "Nathalia@Nathalia.com",
-                "3104",
-                Arrays.asList(anime2)
-        );
-
-        userService.saveAll(Arrays.asList(user1, user2, user3));
-
-
-        Score score1 = new Score(user1, anime1, 10);
-        Score score2 = new Score(user1, anime2, 5);
-        Score score3 = new Score(user3, anime2, 8);
-        scoreService.saveAll(Arrays.asList(score1, score2, score3));
-    }
+    @Bean
+    public void initiateDatabase(){ databaseInitializer.initDB(); }
 }
---
+++ src/main/java/com/example/demo/database/DatabaseInitializer.java
@@ -0,0 +1,82 @@
+package com.example.demo.database;
+
+import com.example.demo.entities.Anime;
+import com.example.demo.entities.Score;
+import com.example.demo.entities.User;
+import com.example.demo.services.AnimeService;
+import com.example.demo.services.ScoreService;
+import com.example.demo.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+@RequiredArgsConstructor
+public class DatabaseInitializer {
+
+    private final AnimeService animeService;
+    private final UserService userService;
+    private final ScoreService scoreService;
+
+    public void initDB()
+    {
+        Anime anime1 = new Anime(
+                null,
+                "Spy x Family",
+                new String[]{"Clover Works", "Wit Studio"},
+                new String[]{"Comedy", "Romance", "Slice of life"},
+                "Tatsuya Endo"
+        );
+
+        Anime anime2 = new Anime(
+                null,
+                "One Piece",
+                new String[]{"Toei Animation"},
+                new String[]{"Comedy", "Shounen", "Adventure"},
+                "Eichiro Oda"
+        );
+
+        Anime anime3 = new Anime(
+                null,
+                "Steins;Gate",
+                new String[]{"White Fox"},
+                new String[]{"Sci-fi", "Thriller", "Drama"},
+                "Jukki Hanada"
+        );
+
+        animeService.saveAll(Arrays.asList(anime1, anime2, anime3));
+
+        User user1 = new User(
+                null,
+                "Luiz Adelaide",
+                "Luiz@Luiz.com",
+                "2241",
+                Arrays.asList(anime2, anime1)
+        );
+
+        User user2 = new User(
+                null,
+                "Joao Camara",
+                "Joao@Joao.com",
+                "6666",
+                Arrays.asList(anime1)
+        );
+
+        User user3 = new User(
+                null,
+                "Nathalia Adelaide",
+                "Nathalia@Nathalia.com",
+                "3104",
+                Arrays.asList(anime2)
+        );
+
+        userService.saveAll(Arrays.asList(user1, user2, user3));
+
+
+        Score score1 = new Score(user1, anime1, 10);
+        Score score2 = new Score(user1, anime2, 5);
+        Score score3 = new Score(user3, anime2, 8);
+        scoreService.saveAll(Arrays.asList(score1, score2, score3));
+    }
+}
