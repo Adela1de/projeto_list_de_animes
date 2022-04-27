c3ebc6c2896fb94101a1d99d2763311434e12078
Adela1de <94483883+Adela1de@users.noreply.github.com>
2022-04-27 12:29:33 -0300

    Adding favorites to UserPutRequestBody

--- src/main/java/com/example/demo/requests/userrequests/UserPutRequestBody.java
+++ src/main/java/com/example/demo/requests/userrequests/UserPutRequestBody.java
@@ -1,4 +1,14 @@
 package com.example.demo.requests.userrequests;
 
+import com.example.demo.entities.Anime;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class UserPutRequestBody  extends UserEntityCamps{
+
+    private List<Anime> favorites;
 }
